f6700de2aa9d7cb2423422c58f1feed230c16a0f
Nena <nena@192.168.0.114.non-exists.ptr.local>
2024-12-24 22:52:31 +0700

    lesson of day-01

---
+++ lessons/day-01/VariableLearning.js
@@ -0,0 +1,29 @@
+// Variable 
+// = assignment operand
+// Declaration
+let a = 1;
+
+// X re-decalration is NOT allowed 
+// Re-assign is acceptable 
+a = 2;
+
+// constant
+const PI_NUMBER = 3.14;
+// Not allowed to reassign 
+// PI_NUMBER = 3.23;
+
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308 = 1.7976931348623157 * 10^38
+console.log(Number.MIN_VALUE); // 5e-324
+
+// Check the type
+console.log(typeof(1.7976931348623157e+308 + 1));
+console.log(0.1 + 0.2);
+
+// Boolean
+let amIright = false; // operand
+console.log(typeof amIright);
+
+
+
+
+
